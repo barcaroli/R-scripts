ba6baa998b4f94760fc42826801f2bac26fb3c00
barcaroli <gbarcaroli@gmail.com>
2019-08-27 18:41:19 +0200

    meuse case study

--- Case study 1 multivariate.R
+++ Case study 1 multivariate.R
@@ -1,14 +1,12 @@
 #-------------------------------------------------------------------
 # Optimization of Spatial Sampling with the R packageSamplingStrata"
 # R script for case study 1: the "meuse" dataset
-# Multivariate case (only one domain)
+# Multivariate case 
 #-------------------------------------------------------------------
 
-library(lattice) # for meuse and meuse.grid
 library(sp)
 library(gstat)
 library(automap)
-library(ggplot2)
 library(SamplingStrata)
 
 # locations (155 observed points)
@@ -26,59 +24,64 @@ meuse.grid <- as(meuse.grid, "SpatialPixelsDataFrame")
 # Cadmium
 v <- variogram(cadmium ~ dist + soil, data=meuse, cutoff=3000, width=3000/30)
 plot(v)
-fit.vgm = autofitVariogram(cadmium ~ dist + soil, meuse, model = "Exp")
+fit.vgm = autofitVariogram(cadmium ~ dist + soil, meuse, 
+                           model = c("Sph","Exp"))
 plot(v, fit.vgm$var_model)
 fit.vgm$var_model
-# model     psill    range
-# 1   Nug  2.713204   0.0000
-# 2   Exp 12.166605 397.6111
+# model    psill    range
+# 1   Nug 2.821884   0.0000
+# 2   Exp 5.121428 240.6374
 
 # Copper
 v <- variogram(copper ~ dist + soil, data=meuse, cutoff=3000, width=3000/30)
 plot(v)
-fit.vgm = autofitVariogram(copper ~ dist + soil, meuse, model = "Exp")
+fit.vgm = autofitVariogram(copper ~ dist + soil, meuse,  
+                           model = c("Sph","Exp"))
 plot(v, fit.vgm$var_model)
 fit.vgm$var_model
-# model     psill    range
-# 1   Nug  70.34369   0.0000
-# 2   Exp 546.74507 254.2308
+# model    psill   range
+# 1   Nug   0.0000  0.0000
+# 2   Exp 339.6246 98.4095
 
 # Lead
 v <- variogram(lead ~ dist + soil, data=meuse, cutoff=3000, width=3000/30)
 plot(v)
-fit.vgm = autofitVariogram(lead ~ dist + soil, meuse, model = "Exp")
+fit.vgm = autofitVariogram(lead ~ dist + soil, meuse,   
+                           model = c("Sph","Exp"))
 plot(v, fit.vgm$var_model)
 fit.vgm$var_model
-# model     psill    range
-# 1   Nug  2590.284   0.0000
-# 2   Exp 14803.573 520.6309
+# model    psill    range
+# 1   Nug 3738.507    0.000
+# 2   Sph 6243.891 1020.692
 
 # Zinc
 v <- variogram(zinc ~ dist + soil, data=meuse, cutoff=3000, width=3000/30)
 plot(v)
-fit.vgm = autofitVariogram(zinc ~ dist + soil, meuse, model = "Exp")
+fit.vgm = autofitVariogram(zinc ~ dist + soil, meuse,   
+                           model = c("Sph","Exp"))
 plot(v, fit.vgm$var_model)
 fit.vgm$var_model
-# model     psill  range
-# 1   Nug  14255.14   0.00
-# 2   Exp 160448.65 416.04
+# model    psill    range
+# 1   Nug 25328.92   0.0000
+# 2   Exp 67450.62 400.5704
+
 # multivariate case:cadmium, copper, lead, zinc as target variables
 g <- NULL
 g <- gstat(NULL, "Cd", cadmium ~ dist + soil, meuse)
 g <- gstat(g, "Cu", copper ~ dist + soil, meuse)
 g <- gstat(g, "Pb", lead ~ dist + soil, meuse)
 g <- gstat(g, "Zn", zinc ~ dist + soil, meuse)
-g
 vm <- variogram(g)
-vm.fit <- fit.lmc(vm, g, vgm(psill=1, model="Sph", range=800, nugget=1))
+# Using "Sph" because "Exp" does not work
+vm.fit <- fit.lmc(vm, g, vgm(psill=1, model=c("Sph"), range=800, nugget=1))
 vm.fit
 plot(vm, vm.fit)
-
 preds <- predict(vm.fit, meuse.grid)
 names(preds)
 # [1] "Cd.pred"   "Cd.var"    "Cu.pred"   "Cu.var"    "Pb.pred"  
 # [6] "Pb.var"    "Zn.pred"   "Zn.var"    "cov.Cd.Cu" "cov.Cd.Pb"
 # [11] "cov.Cu.Pb" "cov.Cd.Zn" "cov.Cu.Zn" "cov.Pb.Zn"
+
 preds$Cd.pred <- ifelse(preds$Cd.pred < 0,0,preds$Cd.pred)
 preds$Cu.pred <- ifelse(preds$Cu.pred < 0,0,preds$Cu.pred)
 preds$Pb.pred <- ifelse(preds$Pb.pred < 0,0,preds$Pb.pred)
@@ -133,15 +136,11 @@ frame$var4 <- df$Zn.var
 kmeans <- KmeansSolutionSpatial(frame,
                                 fitting=1,
                                 range=800,
-                                gamma=3,
+                                gamma=1,
                                 errors=cv,
-                                maxclusters = 5)
-# sugg <- prepareSuggestion(frame,range=800,suggestions = kmeans$suggestions)
-# sugg
-# 
-# frameres1 <- SpatialPointsDataFrame(data=framenew, coords=cbind(framenew$lon,framenew$lat) )
-# frameres1$LABEL <- as.factor(frameres1$LABEL)
-# spplot(frameres1,"LABEL")
+                                maxclusters = 10)
+# The difference in sample size after nStrata = 5 is negligible
+# so we choose nStrata = 5
 
 #---------------------------------------------------
 # Solution with spatial optimization on continuous variables
@@ -154,38 +153,46 @@ solution <- optimizeStrataSpatial (
   iter = 50,
   pops = 10,
   nStrata = 5,
+  fitting = 1,
   range = 800,
+  gamma = 1,
   writeFiles = FALSE,
   showPlot = TRUE,
   parallel = FALSE
 )
-save(solution,file="solution_meuse_nodom.RData")
-sum(solution$aggr_strata$SOLUZ)
+sum(round(solution$aggr_strata$SOLUZ))
 
 
-expected_CV(solution$aggr_strata)
-# cv(Y1) cv(Y2) cv(Y3) cv(Y4)
-# DOM1   0.05  0.028  0.033  0.033
 ss <- summaryStrata(x=solution$framenew,outstrata=solution$aggr_strata)
-sink("./output/ssmulti.txt")
+# sink("./output/ssmulti.txt")
 ss
-sink()
+# sink()
 sum(ss$Allocation)
 framenew <- solution$framenew
 framenew$lon <- meuse.grid@coords[,1]
 framenew$lat <- meuse.grid@coords[,2]
 # framenew$strat <- paste(framenew$DOMAINVALUE,framenew$LABEL,sep="")
 table(framenew$LABEL)
+
+# Analysis of CVs
+expected_CV(solution$aggr_strata)
+# cv(Y1) cv(Y2) cv(Y3) cv(Y4)
+# DOM1   0.05  0.028  0.033  0.033
+
+# Simulation
+val <- evalSolution(solution$framenew,solution$aggr_strata,nsampl=30)
+val$coeff_var     
+# CV1  CV2    CV3    CV4  dom
+# 1 0.0173 0.01 0.0146 0.0134 DOM1
+
 # selection of sample
 sample <- selectSample(frame=framenew,outstrata=solution$aggr_strata)
 head(sample)
 
+# Plot
 frameres2 <- SpatialPointsDataFrame(data=framenew, coords=cbind(framenew$LON,framenew$LAT) )
 frameres3 <- SpatialPixelsDataFrame(points=frameres2[c("LON","LAT")], data=framenew)
 frameres3$LABEL <- as.factor(frameres2$LABEL)
 spplot(frameres3,c("LABEL"), col.regions=bpy.colors(5))
 
-
-expected_CV(solution$aggr_strata)
-val <- evalSolution(solution$framenew,solution$aggr_strata,nsampl=30)
-val$coeff_var                  
+# save(solution,file="solution_meuse_multi_5strata.RData")
--- Case study 1 univariate.R
+++ Case study 1 univariate.R
@@ -4,12 +4,9 @@
 # Univariate case
 #-------------------------------------------------------------------
 
-library(lattice) # for meuse and meuse.grid
 library(sp)
 library(gstat)
 library(automap)
-library(magrittr)
-library(ggplot2)
 library(SamplingStrata)
 
 # locations (155 observed points)
@@ -19,8 +16,7 @@ summary(meuse)
 data("meuse.grid")
 meuse.grid$id <- c(1:nrow(meuse.grid))
 cor(meuse[,c(3:6)])
-par(mfrow=c(1,2))
-png("./images/meuse.png")
+# png("./images/meuse.png")
 par(mfrow=c(1,2))
 plot(meuse.grid$x,meuse.grid$y,
      xlab="Longitude",
@@ -30,15 +26,16 @@ plot(meuse$x,meuse$y,
      xlab="Longitude",
      ylab="Latitude")
 title("meuse dataset")
-dev.off()
+par(mfrow=c(1,1))
+# dev.off()
 
 coordinates(meuse)<-c("x","y")
 coordinates(meuse.grid)<-c("x","y")
 
 
-png("./images/lead.png")
+# png("./images/lead.png")
 spplot(meuse,"lead",do.log=F,ylab="Lead localisation and concentration")
-dev.off()
+# dev.off()
 spplot(meuse.grid,"soil")
 levels(meuse.grid$soil)
 bubble(meuse,"lead",do.log=F,key.space="bottom")
@@ -46,52 +43,50 @@ meuse.grid <- as(meuse.grid, "SpatialPixelsDataFrame")
 meuse.grid@bbox
 ha <- (meuse.grid@bbox[1,2]-meuse.grid@bbox[1,1]) * (meuse.grid@bbox[2,2]-meuse.grid@bbox[2,1]) / 20000 
 ha
-v <- variogram(lead ~ 1, data=meuse, cutoff=2000, width=2000/20)
-fit.vgm = autofitVariogram(lead ~ 1, meuse, model = "Exp")
+#################
+# kriging
+#################
+
+v <- variogram(lead ~ dist + soil, data=meuse)
+fit.vgm <- autofitVariogram(lead ~ elev + soil, meuse, model = "Exp")
 plot(v, fit.vgm$var_model)
 fit.vgm$var_model
-# model     psill    range
-# 1   Nug  2590.284   0.0000
-# 2   Exp 14803.573 520.6309
-
-# univariate case: lead as target variable
-g <- gstat(NULL, "Pb", lead ~ 1, meuse)
-# g <- gstat(g, "Cu", copper ~ 1, meuse)
-# g <- gstat(g, "Pb", lead ~ 1, meuse)
-# g <- gstat(g, "Zn", zinc ~ 1, meuse)
+# model    psill    range
+# 1   Nug 1524.895   0.0000
+# 2   Exp 8275.431 458.3303
+g <- gstat(g,"Pb", lead ~ dist + soil, meuse)
 g
 vm <- variogram(g)
-vm.fit <- fit.lmc(vm, g, 
-                  vgm(psill=fit.vgm$var_model$psill[2], 
-                      model="Exp", 
-                      range=fit.vgm$var_model$range[2], 
-                      nugget=fit.vgm$var_model$psill[1]))
+vm.fit <- fit.lmc(vm, g, vgm(psill=fit.vgm$var_model$psill[2], 
+                             model="Exp", range=fit.vgm$var_model$range[2], 
+                             nugget=fit.vgm$var_model$psill[1]))
 
+# Evaluation of fitting
+# preds <- predict(vm.fit, meuse)
+# meuse$Pb.pred <- preds$Pb.pred
+# lm1 <- lm(lead ~ Pb.pred, meuse)
+# summary(lm1)
+# summary(lm1)$r.squared
 
+# Prediction on the whole grid
 preds <- predict(vm.fit, meuse.grid)
 names(preds)
+# [1] "Pb.pred" "Pb.var"
 summary(preds$Pb.pred)
 preds$Pb.pred <- ifelse(preds$Pb.pred < 0,0,preds$Pb.pred)
 summary(preds$Pb.pred)
 df <- NULL
-# df$Cd.pred <- preds@data$Cd.pred
-# df$Cd.var <- preds@data$Cd.var
-# df$Cu.pred <- preds@data$Cu.pred
-# df$Cu.var <- preds@data$Cu.var
 df$Pb.pred <- preds@data$Pb.pred
 df$Pb.var <- preds@data$Pb.var
-# df$Zn.pred <- preds@data$Zn.pred
-# df$Zn.var <- preds@data$Zn.var
-# df$dom <- meuse.grid@data$soil
 df$dom1 <- 1
 df <- as.data.frame(df)
 df$id <- meuse.grid$id
 
 
 
-#-----------------------------------------------------------
-# Solution with spatial optimization on continuous variables
-#-----------------------------------------------------------
+#------------------------
+# Solution SamplingStrata
+#------------------------
 frame <- buildFrameDF(df=df,
                       id="id",
                       X=c("Pb.pred"),
@@ -117,7 +112,7 @@ solution <- optimizeStrataSpatial (
   nStrata = 3,
   fitting = 1,
   range = fit.vgm$var_model$range[2],
-  gamma=3,
+  gamma=1,
   writeFiles = FALSE,
   showPlot = TRUE,
   parallel = FALSE
@@ -127,21 +122,20 @@ sum(round((solution$aggr_strata$SOLUZ)))
 
 # expected_CV(solution$aggr_strata)
 ss <- summaryStrata(x=solution$framenew,outstrata=solution$aggr_strata)
-sink("./output/ss.txt")
+# sink("./output/ss.txt")
 ss
-sink()
+# sink()
 framenew <- solution$framenew
 outstrata <- solution$aggr_strata
 framenew$lon <- meuse.grid@coords[,1]
 framenew$lat <- meuse.grid@coords[,2]
 
-plotStrata2d(framenew,outstrata,domain=1,vars=c("X1","X1"))
+# plotStrata2d(framenew,outstrata,domain=1,vars=c("X1","X1"))
 
 s <- selectSample(framenew,outstrata)
 
 expected_CV(outstrata)
 val1 <- evalSolution(framenew,outstrata,nsampl=1000,progress=F)
-
 val1$coeff_var 
 
 frameres2 <- SpatialPointsDataFrame(data=framenew, coords=cbind(framenew$LON,framenew$LAT) )
@@ -149,16 +143,17 @@ frameres3 <- SpatialPixelsDataFrame(points=frameres2[c("lon","lat")], data=frame
 frameres3$LABEL <- as.factor(frameres2$LABEL)
 spplot(frameres3,c("LABEL"), col.regions=bpy.colors(5))
 
-#-----------------------------------------------------------
-# Solution with ospats
-#-----------------------------------------------------------
+#---------------------
+# Solution with Ospats
+#---------------------
 
-#------------------ Ospats processing
+#------------------ Ospats processing -----
+# N.B. : use only Julia 0.6.4 
+#------------------------------------------
 setwd("./ospats")
 # devtools::install_github("Non-Contradiction/JuliaCall")
 library(JuliaCall)
 julia <- julia_setup()
-# julia <- julia_setup(JULIA_HOME = "C:\\Users\\Giulio\\AppData\\Local\\Julia-0.6.4\\bin")
 #-------------------------------------
 add_variance <- 0
 file <- NULL
@@ -167,16 +162,13 @@ file$y <- frame$lat
 file$id <- c(1:nrow(frame))
 file$z <- frame$Y1
 file$var <- frame$var1+add_variance
-# file$stratum <- frame$LABEL
 file <- as.data.frame(file)
-# file <- file[order(paste(file$x,file$y,sep="")),]
 write.table(file,"example.txt",row.names=F,col.names=F,sep=",",quote=F)
 #--------------------------------------------------------
 julia_console()
 include("main.jl")
 exit
 #--------------------------------------------------------
-# fr <- read.table("stratification",sep="\t",header=FALSE)
 fr <- read.table("stratification",sep=",",header=TRUE,dec=".",stringsAsFactors = FALSE)
 colnames(fr) <- c("lon","lat","stratum")
 table(fr$stratum)
@@ -188,24 +180,41 @@ strata <- aggrStrataSpatial(dataset=frameone,
                             gamma = 1,
                             dominio = 1)
 strata$allocation <- bethel(strata,cv)
-sink("./output/ospats_strata.txt")
+# sink("./output/ospats_strata.txt")
 strata
-sink()
+# sink()
 # stratum    N COST CENS DOM1        M1       S1 allocation
 # 1       1 1065    1    0    1 143.00822 73.59579         43
 # 2       2  677    1    0    1 254.62742 87.53205         33
 # 3       3 1360    1    0    1  61.96497 71.99991         53
-solution$aggr_strata
-sum(bethel(strata,cv))
-sum(bethel(solution$aggr_strata,cv))
-table(framenew$LABEL)
-table(fr$stratum)
+
+# Plot
 spfr <- SpatialPointsDataFrame(data=fr, coords=cbind(fr$lon,fr$lat) )
 spfr2 <- SpatialPixelsDataFrame(points=spfr[c("lon","lat")], data=fr)
 spfr2$Ospats <- as.factor(fr$stratum)
 spplot(spfr2,c("Ospats"), col.regions=bpy.colors(5))
+#---------------------------------
+# Comparison of results
+#---------------------------------
+# Ospats
+# sink("./output/ospats_strata.txt")
+strata
+# sink()
+# stratum    N COST CENS DOM1        M1       S1 allocation
+# 1       1 1065    1    0    1 143.00822 73.59579         43
+# 2       2  677    1    0    1 254.62742 87.53205         33
+# 3       3 1360    1    0    1  61.96497 71.99991         53
+sum(bethel(strata,cv))
+table(fr$stratum)
+#---------------------------------
+# SamplingStrata
+solution$aggr_strata
+# STRATO    N        M1       S1 COST CENS DOM1 X1    SOLUZ
+# 1      1 1360  61.96126 72.01323    1    0    1  1 52.86037
+# 2      2 1073 143.41700 73.77912    1    0    1  2 42.72796
+# 3      3  670 255.32415 87.43633    1    0    1  3 31.61882
+sum(bethel(solution$aggr_strata,cv))
+table(framenew$LABEL)
 
-
-
-save.image(file="lead_4strata.RData")
+# save.image(file="lead_3strata.RData")
 
